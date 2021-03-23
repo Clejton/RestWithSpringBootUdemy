0ed08c5a78e78664d930b10dd426b7fe62920230
Clejton <cleiton.freitas1@gmail.com>
2021-03-23 16:53:59 -0400

    Incluindo docker-compose.yml

--- 03 RestWithSpringBootUdemy Scaffold Verbos/src/main/java/br/com/erudio/services/PersonServices.java
+++ 03 RestWithSpringBootUdemy Scaffold Verbos/src/main/java/br/com/erudio/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio.services;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
@@ -17,6 +19,25 @@ public class PersonServices {
         person.setLastName("Freitas");
         person.setAddress("Nova Mutum - Mato Grosso - Brasil");
         person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0 ; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name "  + i);
+        person.setAddress("Some address in Brazil "  + i);
+        person.setGender("Male "  + i);
         return person;
     }
 
